4a55cf3c4dc828cf7eacf3d24f96e8317feef312
Douglas Brito Da Rocha <oceaniluminat@gmail.com>
2025-08-24 18:03:17 -0300

    Update historico.js

--- historico.js
+++ historico.js
@@ -1,17 +1,22 @@
 function listarHistorico(){
   let historico = JSON.parse(localStorage.getItem("historico")) || [];
+  let filtro = document.getElementById("filtro")?.value || "Todos";
+
   let tabela = document.querySelector("#tabela tbody");
   tabela.innerHTML = "";
-  historico.forEach(h => {
-    let row = `<tr>
-      <td>${h.data}</td>
-      <td>${h.usuario}</td>
-      <td>${h.produto}</td>
-      <td class="tipo-${h.tipo}">${h.tipo}</td>
-      <td>${h.quantidade}</td>
-    </tr>`;
-    tabela.innerHTML += row;
-  });
+
+  historico
+    .filter(h => filtro === "Todos" || h.tipo === filtro)
+    .forEach(h => {
+      let row = `<tr>
+        <td>${h.data}</td>
+        <td>${h.usuario}</td>
+        <td>${h.produto}</td>
+        <td class="tipo-${h.tipo}">${h.tipo}</td>
+        <td>${h.quantidade}</td>
+      </tr>`;
+      tabela.innerHTML += row;
+    });
 }
 
 function logout(){
